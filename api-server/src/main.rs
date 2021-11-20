bc4ebefcf4768858250771958d501d4de0371132
behike56 <living.tech.dancing.code@gmail.com>
2021-11-20 11:50:58 +0900

    initial commit

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,28 +1,45 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer};
+
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "WELCOME";
+    Ok(HttpResponse::Ok().body(response_body))
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(hello)
+//             .service(echo)
+//             .route("/hey", web::get().to(manual_hello))
+//     })
+//     .bind("127.0.0.1:8080")?
+//     .run()
+//     .await
+// }
+
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
+
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
